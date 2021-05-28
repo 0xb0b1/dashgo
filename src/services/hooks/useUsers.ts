014122d8aff78be61ae63dfb1ae8e41b389860e2
0xb0b1 <vcente82@gmail.com>
2021-05-28 16:30:06 -0300

    ...

---
+++ src/services/hooks/useUsers.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "react-query";
+import { api } from "../api";
+
+interface User {
+  map(arg0: (user: User) => JSX.Element): import("react").ReactNode;
+  id: number;
+  name: string;
+  email: string;
+  createdAt: string;
+}
+
+export async function getUsers(): Promise<User> {
+  const { data } = await api.get("/users");
+
+  const users = data.users.map((user: User) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
+    };
+  });
+
+  return users;
+}
+
+export function useUsers() {
+  return useQuery("users", getUsers, {
+    staleTime: 1000 * 5,
+  });
+}
