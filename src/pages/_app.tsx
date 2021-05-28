df66547e3d0d6efdbf592ec44bf03a6e1867f55a
0xb0b1 <vcente82@gmail.com>
2021-05-28 16:29:45 -0300

    providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,31 @@
 import { AppProps } from "next/app";
+
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../styles/theme";
 import { MenuProvider } from "../contexts/MenuDrawerContext";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { theme } from "../styles/theme";
+import { makeServer } from "../services/mirage";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <MenuProvider>
-        <Component {...pageProps} />
-      </MenuProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <MenuProvider>
+          <Component {...pageProps} />
+        </MenuProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
